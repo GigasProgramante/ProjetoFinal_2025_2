228c8ec3ff81be9e1dedfe2ba05ced619a8a05f1
Guilherme Vojciechovski <guilhermevoj@gmail.com>
2025-11-27 16:19:16 -0300

    Adionei os ENUNS
    (cherry picked from commit a3b8c6ffb02b4c9c314d1ca3c06a926d32eaba3f)

---
+++ src/main/Cargos.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum Cargos {
+    RECEPCIONISTA, GERENTE, ZELADOR, AULIAR_ADMINISTRATIVO, CONTADOR
+}
---
+++ src/main/StatusConsulta.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum StatusConsulta {
+    AGENDADA, CONCLUIDA, CANCELADA
+}
