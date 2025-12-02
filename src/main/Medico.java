d2141bbda61a5c1fac4f902edbe3d21e0cc8983c
Guilherme Vojciechovski <guilhermevoj@gmail.com>
2025-12-02 19:57:31 -0300

    Id Random

--- src/main/Medico.java
+++ src/main/Medico.java
@@ -12,6 +12,11 @@ public class Medico extends Pessoa {
         this.crm = crm;
         this.salario = salario;
     }
+
+    public void criarConsulta() {
+
+    }
+
     @Override
     public String exibirInfo() {
         return super.exibirInfo() + "~~~~~~~~~~~ Médico:" +
--- src/main/Prontuario.java
+++ src/main/Prontuario.java
@@ -1,21 +1,24 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Prontuario {
 
-    private int numero;
+    private int idProntuario;
     private Consulta consulta;
     private ArrayList<Consulta> consultas;
 
     private ArrayList<Receita> receitas;
     private Paciente paciente;
 
-    public Prontuario(int numero, Consulta consulta, ArrayList<Consulta> consultas, Paciente paciente) {
-        this.numero = numero;
+    public Prontuario(Consulta consulta, ArrayList<Consulta> consultas, Paciente paciente) {
+        Random rd = new Random();
+        this.idProntuario = rd.nextInt();
         this.consulta = consulta;
         this.consultas = consultas;
         this.paciente = paciente;
+
     }
     public void adicionarReceita(Receita receita){
         this.receitas.add(receita);
@@ -24,7 +27,7 @@ public class Prontuario {
         this.receitas.remove(receita);
     }
     public void exibirReceitas() {
-        System.out.println("Receitas no Prontuário " + numero + " do Paciente " + (paciente != null ? paciente.exibirInfo() : "Desconhecido") + ":");
+        System.out.println("Receitas no Prontuário " + idProntuario + " do Paciente " + (paciente != null ? paciente.exibirInfo() : "Desconhecido") + ":");
         for (Receita r : receitas) {
             r.exibirDetalhes();
         }
